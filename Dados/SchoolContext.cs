be24eca189af2a86a387dea54fc94571408430e2
jonathan <jonathanlemos@outlook.com>
2020-05-25 02:12:02 -0300

    Inicio

---
+++ Dados/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ContosoUniversity.Dados
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        //protected override void OnModelCreating(Model modelBuilder)
+        //{
+        //    modelBuilder.Entity<Course>().ToTable("Course");
+        //    modelBuilder.Entity<Enrollment>().ToTable("Course");
+        //    modelBuilder.Entity<Student>().ToTable("Course");
+        //}
+
+    }
+}
